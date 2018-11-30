99a81cf56f9c41751cf902d5277fb361b09a732a
Mr DaHoM <44880953+mrdahom4@users.noreply.github.com>
2018-11-30 13:22:00 +0200

    Update bot .js

--- bot .js
+++ bot .js
@@ -100,18 +100,7 @@ client.on('message', msg => {
   }
 });
 
-client.on("message", message => {
-    if (message.content.startsWith(prefix + "bc")) {
-                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' ');
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
-  m.send(`${argresult}\n ${m}`);
-  })
-  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:love_letter:  **عدد المستلمين** `);
-  message.delete();
-  };
-  });
+
 
 client.on('message', msg => {
   if (msg.content === 'باك') {
